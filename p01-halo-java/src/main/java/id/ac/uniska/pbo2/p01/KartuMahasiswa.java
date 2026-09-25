b25d5d3e0c2118a516b1c939a7550c08f053675b
Asma Nur Patmi <2410010342Patmi@gmail.com>
2026-09-25 19:32:43 +0800

    P01: menambahkan KartuMahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+public class KartuMahasiswa {
+
+    public static void main(String[] args) {
+
+        String nama = "Asma Nur Patmi";
+        String npm = "2410010342";
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Karena mata kuliah lanjutan dari semester sebelumnya";
+
+        System.out.println("================================");
+        System.out.println("       KARTU MAHASISWA PBO 2");
+        System.out.println("================================");
+        System.out.println("Nama     : " + nama);
+        System.out.println("NPM      : " + npm);
+        System.out.println("Prodi    : " + prodi);
+        System.out.println("Semester : " + semester);
+        System.out.println("Alasan   : " + alasan);
+        System.out.println("================================");
+    }
+}
